5626549069cb9c8c5ca4753ca04d7d40547edfef
joseL32 <jose.orosco@upeu.edu.pe>
2021-12-13 16:41:18 -0500

    version dos

--- APIRestDjango/apirest/admin.py
+++ APIRestDjango/apirest/admin.py
@@ -1,3 +1,9 @@
+from typing import List
 from django.contrib import admin
+from .models import Trabajador
 
-# Register your models here.
+class TrabajadorAdmin(admin.ModelAdmin):
+    list_display =('id', 'nombres', 'dni', 'celular', 'direccion', 'especialidad')
+
+admin.site.register(Trabajador,TrabajadorAdmin)
+
